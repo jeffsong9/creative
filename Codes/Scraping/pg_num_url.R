903c2607f6d5acc39349aaa3f3f9fb83d5a0558d
jeffsong9 <jeffsong9@gmail.com>
2017-09-15 12:31:36 -0500

    Delete old CC
    Trip Advisor Paper deleted

--- Codes/Scraping/pg_num_url.R
+++
@@ -1,9 +0,0 @@
-get.pg.num=function(intial.url){
-  doc1=read_html(initial.url)
-  pg.num.path=c(pg.url = '//div [@class="pageNumbers"]')
-  check=lapply(pg.num.path, function(x) html_nodes(doc1,xpath=x))
-  f.page=check$pg.url %>% html_text() %>% gsub(".*\n(.*)\n", "\\1", .)
-  complete.initial.url=rbind.data.frame(rest.url=initial.url, data.frame(rest.url=sapply(c(1:(as.numeric(f.page)-1))*30, function(x) gsub("SUB", x,"https://www.tripadvisor.com/Restaurants-g60982-oaSUB-Honolulu_Oahu_Hawaii.html#EATERY_LIST_CONTENTS")), stringsAsFactors = F), stringsAsFactors = F)
-  row.names(complete.initial.url)=NULL
-  return(complete.initial.url[[1]])
-}
--- Codes/Scraping/review_data_w_pb.R
+++
@@ -1,5 +0,0 @@
-pb <- winProgressBar(title = "progress bar", min = 0, max = fin, width = 300)
-review.data.w.pb=function(x){
-  setWinProgressBar(pb, x, title=paste(round(x/fin*100, 0), "% done"))
-  return(review.data(ea.url[x,1]))
-}
